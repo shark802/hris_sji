f08ed5ac589a9546c29e5c9afd7bd78c4e4003d7
JsOn de los Reyes <jasondelosreyes914@gmail.com>
2022-10-17 08:14:29 +0800

    update database connection

--- admin/s/db_conn/conn.php
+++ admin/s/db_conn/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'hris_sji');
+	$conn = new mysqli('139.177.190.17', 'j@son', 'j@$0n%$h@rKDB', 'hris_sji');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
